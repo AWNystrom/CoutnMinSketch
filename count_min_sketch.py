e589cd9e2afc220b3a175fe692c70b194bbfd03f
Andrew Nystrom <awnystrom@gmail.com>
2014-09-27 10:14:46 -0500

    using fnv hash

--- count_min_sketch.py
+++ count_min_sketch.py
@@ -2,8 +2,20 @@ from numpy import zeros, int32, arange, array
 from hashlib import md5
 from math import log, e, ceil
 
+import lru_cacher
+
+def fnv64(a_string):
+	in_bytes = bytearray(a_string)
+	h = 0xcbf29ce484222325
+	for byte in in_bytes:
+		h *= 0x100000001b3
+		h &= 0xffffffffffffffff
+		h ^= byte
+	return h
+
 class CountMinSketch(object):
-	def __init__(self, w=None, d=None, delta=None, epsilon=None, f=md5):
+	def __init__(self, w=None, d=None, delta=None, epsilon=None, f=fnv64,
+							cache_size=0):
 		"""
 		CountMinSketch is an implementation of the count min sketch 
 		algorithm that probabilistically counts string frequencies.
@@ -12,8 +24,9 @@ class CountMinSketch(object):
 		----------
 		w : the number of columns in the count matrix
 		d : the number of rows in the count matrix
-		f : the hash function class to be used. Must have the method hexdigest
-		epsilon : the error factor.
+		f : the hash function class to be used. Must be callable. By default
+				the fnv64 hash function is used, which is very fast and good for
+				w values less than 2^64.
 		delta : the probability the error factor will hold.
 		For the full paper on the algorithm, see the paper
 		"An improved data stream summary: the count-min sketch and its -
@@ -28,19 +41,21 @@ class CountMinSketch(object):
 		self.f = f
 		self.count = zeros((self.d, self.w), dtype=int32)
 		self.rows = arange(self.d)
+		self.cache_size = cache_size
+		self.cache = lru_cacher.LruCacher(cache_size, self.get_columns)
 
 	def get_columns(self, a):
 		w = self.w
 		s = str(a)
 		f = self.f
-		return array([int(f(s+str(i)).hexdigest(), 16) % w for i in xrange(self.d)])
+		return array([f(str(i)+s) % w for i in xrange(self.d)])
 		
 	def update(self, a, val=1):
 		h = self.get_columns(a)
 		self.count[self.rows, h] += val
 	
 	def query(self, a):
-		h = self.get_columns(a)
+		h, in_cache = self.cache.lookup(a)
 		return self.count[self.rows, h].min()
 	
 	def __getitem__(self, a):
