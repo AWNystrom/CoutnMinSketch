151b623e1fbbfe41eb8400d00b6ca7ff838afa60
Andrew Nystrom <awnystrom@gmail.com>
2014-10-04 01:16:41 -0500

    speed test uses each letter of alphabet now:

--- count_min_sketch/speed_test.py
+++ count_min_sketch/speed_test.py
@@ -1,21 +1,38 @@
 import countminsketch
 import count_min_sketch
 from time import time
-
+from random import randint
 mytime = 0
 theirtime = 0
 
 mine = count_min_sketch.CountMinSketch(1000, 5000)
 theirs = countminsketch.CountMinSketch(1000, 5000)
 
-for i in xrange(10000):
+from string import letters
+letters = letters[:26]
+to_inc = [randint(5, 100) for letter in letters]
+
+for letter, incto in zip(letters, to_inc):
+
+	for i in xrange(incto):
+		t = time()
+		mine.update(letter)
+		mytime += time() - t
+	
+		t = time()
+		theirs.add(letter)
+		theirtime += time() - t
+
+for letter, should_be in zip(letters, to_inc):
 	t = time()
-	mine.update('a')
+	a = mine[letter]
 	mytime += time() - t
 	
 	t = time()
-	theirs.add('a')
+	b = theirs[letter]
 	theirtime += time() - t
 	
-print mine['a'], theirs['a']
+	print a, b, a == b, a == should_be
+
 print mytime, theirtime
+print theirtime / mytime
