85a8c6082a408cfcab695bdbbef3574cf13629d0
Andrew Nystrom <awnystrom@gmail.com>
2014-10-04 17:18:50 -0500

    Added bits as a para, speed test fetches data

--- count_min_sketch/count_min_sketch.py
+++ count_min_sketch/count_min_sketch.py
@@ -2,8 +2,7 @@ from numpy import zeros, int32, int16, arange, array
 from math import log, e, ceil
 
 class CountMinSketch(object):
-	def __init__(self, w=None, d=None, delta=None, epsilon=None):#,
-							#cache_size=0):
+	def __init__(self, w, d, bits):
 		"""
 		CountMinSketch is an implementation of the count min sketch 
 		algorithm that probabilistically counts string frequencies.
@@ -13,11 +12,38 @@ class CountMinSketch(object):
 		w : the number of columns in the count matrix
 		d : the number of rows in the count matrix
 
+		bits : The size of the hash output space
 		For the full paper on the algorithm, see the paper
 		"An improved data stream summary: the count-min sketch and its -
 		applications" by Cormode and Muthukrishnan, 2003.
 		"""
 		
+		if 2**bits < w:
+			raise Exception("Too few bits for w")
+
+		#Values taken from http://www.isthe.com/chongo/tech/comp/fnv/		
+		if bits == 32:
+			self.prime = 0x1000193
+			self.offset = 0x811c9dc5
+		elif bits == 64:
+			self.prime = 0x100000001b3
+			self.offset = 0xcbf29ce484222325L
+		elif bits == 128:
+			self.prime = 0x1000000000000000000013bL
+			self.offset = 0x6c62272e07bb014262b821756295c58dL
+		elif bits == 256:
+			self.prime = 0x1000000000000000000000000000000000000000163L
+			self.offset = 0xdd268dbcaac550362d98c384c4e576ccc8b1536847b6bbb31023b4c8caee0535L
+		elif bits == 512:
+			self.prime = 0x100000000000000000000000000000000000000000000000000000000000000000000000000000000000157L
+			self.offset = 0xb86db0b1171f4416dca1e50f309990acac87d059c90000000000000000000d21e948f68a34c192f62ea79bc942dbe7ce182036415f56e34bac982aac4afe9fd9L
+		elif bits == 1024:
+			self.prime = 0x10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000018dL
+			self.offset = 0x5f7a76758ecc4d32e56d5a591028b74b29fc4223fdada16c3bf34eda3674da9a21d9000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004c6d7eb6e73802734510a555f256cc005ae556bde8cc9c6a93b21aff4b16c71ee90b3L
+		else:
+			raise Exception("Bits must be 32, 64, 128, 256, 512, or 1024")
+		self.mod = 2**bits-1
+		self.bits = bits
 		self.w = w
 		self.d = d
 		self.count = zeros((self.d, self.w), dtype=int32)
@@ -31,9 +57,9 @@ class CountMinSketch(object):
 		shift_by = self.shift_by
 		hashes = zeros(d, dtype=int16)
 		
-		prime = 0x1000000000000000000000000000000000000000163L
-		offset = 0xdd268dbcaac550362d98c384c4e576ccc8b1536847b6bbb31023b4c8caee0535L
-		mod = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffL
+		prime = self.prime
+		offset = self.offset
+		mod = self.mod
 		
 		#Hash
 		in_bytes = bytearray(a_string)
--- count_min_sketch/speed_test.py
+++ count_min_sketch/speed_test.py
@@ -5,34 +5,71 @@ from random import randint
 mytime = 0
 theirtime = 0
 
-mine = count_min_sketch.CountMinSketch(1000, 5000)
+mine = count_min_sketch.CountMinSketch(1000, 5000, bits=1024)
 theirs = countminsketch.CountMinSketch(1000, 5000)
 
-from string import letters
-letters = letters[:26]
-to_inc = [randint(5, 100) for letter in letters]
+def artificial_test():
+	from string import letters
+	letters = letters[:26]
+	to_inc = [randint(5, 100) for letter in letters]
 
-for letter, incto in zip(letters, to_inc):
+	for letter, incto in zip(letters, to_inc):
 
-	for i in xrange(incto):
+		for i in xrange(incto):
+			t = time()
+			mine.update(letter)
+			mytime += time() - t
+	
+			t = time()
+			theirs.add(letter)
+			theirtime += time() - t
+
+	for letter, should_be in zip(letters, to_inc):
 		t = time()
-		mine.update(letter)
+		a = mine[letter]
 		mytime += time() - t
 	
 		t = time()
-		theirs.add(letter)
+		b = theirs[letter]
 		theirtime += time() - t
-
-for letter, should_be in zip(letters, to_inc):
-	t = time()
-	a = mine[letter]
-	mytime += time() - t
 	
-	t = time()
-	b = theirs[letter]
-	theirtime += time() - t
-	
-	print a, b, a == b, a == should_be
+		print a, b, a == b, a == should_be
 
-print mytime, theirtime
-print theirtime / mytime
+	print mytime, theirtime
+	print theirtime / mytime
+
+def natural_test():
+	from re import findall
+	from collections import defaultdict
+	from urllib2 import urlopen
+	
+	#Adventures of Sherlock Holmes
+	story = urlopen('http://www.gutenberg.org/cache/epub/1661/pg1661.txt').read()
+	tokens = findall('[a-z]{3,}', story)
+	f = defaultdict(lambda: 0)
+	mytime, theirtime = 0, 0
+	for token in tokens:
+		f[token] += 1
+	for i, (token, freq) in enumerate(f.iteritems()):
+		if (i+1)%500 == 0:
+			print 100.*(i+1) / len(f)
+		t = time()
+		mine.update(token, freq)
+		mytime += time() - t
+		
+		t = time()
+		theirs.add(token, freq)
+		theirtime += time() - t
+	
+	right= 0
+	theirright = 0
+	for token, freq in f.iteritems():
+		theirright += theirs.query(token) == freq
+		right += mine.query(token) == freq
+	
+	print 'my % correct:', 1.*right / len(f)
+	print 'their % correct:', 1.*theirright / len(f)
+	print mytime, theirtime, theirtime  / mytime
+
+if __name__ == '__main__':
+	natural_test()
