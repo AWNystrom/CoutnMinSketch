e7b0476f8c0e2f2045fa9f432c65bd3675efefae
Andrew Nystrom <andrew@Andrews-MacBook-Pro-2.local>
2016-03-07 18:06:26 -0600

    cleanup

--- count_min_sketch/count_min_sketch.py
+++ count_min_sketch/count_min_sketch.py
@@ -4,7 +4,7 @@ from math import log, e, ceil
 from itertools import izip
 
 class CountMinSketch(object):
-	def __init__(self, w=None, d=None, delta=None, epsilon=None, bits=256):
+	def __init__(self, w=None, d=None, delta=None, epsilon=None):
 		"""
 		CountMinSketch is an implementation of the count min sketch 
 		algorithm that probabilistically counts string frequencies.
@@ -20,8 +20,6 @@ class CountMinSketch(object):
 		d : the number of rows in the count matrix
 		delta : (not applicable if w and d are supplied) the probability of query error
 		epsilon : (not applicable if w and d are supplied) the query error factor
-
-		bits : The size of the hash output space
 		
 		For the full paper on the algorithm, see the paper
 		"An improved data stream summary: the count-min sketch and its -
@@ -38,9 +36,6 @@ class CountMinSketch(object):
 		else:
 			raise Exception("You must either supply both w and d or delta and epsilon.")
 		
-		if 2**bits < w:
-			raise Exception("Too few bits for w")
-		
 		self.counts = [array('L', (0 for _ in xrange(self.w))) for _ in xrange(self.d)]
 		self.mask = array('L', (randint(0, 2147483647) for _ in xrange(self.d-1)))
 		
@@ -52,31 +47,8 @@ class CountMinSketch(object):
 		for m in self.mask:
 			h ^= m
 			yield h % w
-	
-	def old_get_columns(self, a):
-		h = hash(a)
-		w = self.w
-		
-		#yield h % w
-		for i in xrange(self.d):
-			#h ^= m
-			#yield h % w
-			yield hash((i, h)) % w
-	
-	def old_get_columns(self, a):
-		h = hash(a)#xxh32(a).intdigest()
-		original_h = int(h)
-		w = self.w
-		shift_by = self.shift_by
-		
-		for m in self.mask:
-			yield h % w
-			h >>= shift_by
-			if h < w:# and i < d:
-			  h  = original_h ^ m
 		
 	def update(self, a, val=1):
-		#h, in_cache = self.cache.lookup(a)
 		for row, col in izip(self.counts, self.get_columns(a)):
 		  row[col] += val
 	
